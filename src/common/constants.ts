d82bd972b3cc6abf1fbfc37dc5aacbd6ddafa0ae
Illia Varabyou <veryvorobey@gmail.com>
2022-07-13 15:06:58 +0300

    refactoring

---
+++ src/common/constants.ts
@@ -0,0 +1,2 @@
+export const IMAGE_ALT = 'image error';
+
--- src/pages/Page1/blocks/Header/Header.tsx
+++ src/pages/Page1/blocks/Header/Header.tsx
@@ -1,17 +1,18 @@
 import React from 'react';
 import './Header.scss';
 import logo from "../../../../images/logo-full.svg";
+import {IMAGE_ALT} from "../../../../common/constants";
 
 function Header() {
   return (
     <div className="Header">
-      <img src={logo} className="Header-logo" alt="logo"/>
+      <img src={logo} className="Header-logo" alt={IMAGE_ALT}/>
       <div className="Header-links">
         <span className="Header-link">Page1</span>
         <span className="Header-link">Page2</span>
         <span className="Header-link">Page3</span>
-          <span className="Header-link">Page4</span>
-          <span className="Header-link">Page5</span>
+        <span className="Header-link">Page4</span>
+        <span className="Header-link">Page5</span>
       </div>
       <div className="HeaderMenu">
           <div className="HeaderMenuLine"></div>
